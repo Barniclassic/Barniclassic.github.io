d106442df7098feec7e45c0298582cdd04f8896b
Barni_Classic <76923911+barniclassic@users.noreply.github.com>
2021-09-13 17:41:15 -0500

    W2D5

--- d25destructure/arrayPractice.js
+++ d25destructure/arrayPractice.js
@@ -3,9 +3,6 @@
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
 // module.exports = { doubleNums, doubleAges, filterEven, filterOver10, findEvenNum, findEvenAge, includesEvenNum, includesEvenAge}; //add all of your function names here that you need for the node mocha tests
 
-const numArray = [5,0, 7, 77, -20, 300, 51, 2]
-const peopleArray = [{name: "Sam", age: 15}, {name: "William", age: 6}, {name: "Lucy", age: 13}, {name: 
-"Barney", age: 80}]
 
 function doubleNums(arr){
     return arr.map(num => num * 2);
