1afe09b9ef3186714bc5662631913521fb3b9a9d
Barni_Classic <76923911+barniclassic@users.noreply.github.com>
2021-09-17 23:01:02 -0500

    W3D5

--- d34scheduledCallbacks/callBacks.js
+++ d34scheduledCallbacks/callBacks.js
@@ -2,25 +2,25 @@
 /* eslint-disable require-jsdoc */
 "use strict";
 function timer(from, to){
-  var now = from;
-  var inter = setInterval(function run(){
+  let now = from;
+  let inter = setInterval(function (){
     if(now < to){
-      alert(now);
+      console.log(now);
     }else if(now === to){
       clearInterval(inter);
     }
     now++;}, 1000);
 }
-timer(5, 10);
+timer(0, 10);
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 function counter(from, to){
-  var now = from;
-  var timer = setTimeout(function run(){
+  let now = from;
+  setTimeout(function run(){
     if(now < to){
-      alert(now);
+      console.log(now);
       now++;
-      run(now, 1000);
-    }}, 1000);
+      run(now, 3000);
+    }}, 3000);
 }
 counter(5, 10);
