ab3cc86f49bdd134ae32ce3912cde95830dd7c82
Barni_Classic <76923911+barniclassic@users.noreply.github.com>
2021-09-25 00:55:30 -0500

    Class Project

--- d42class/bank.js
+++ d42class/bank.js
@@ -62,10 +62,6 @@ class Bank {
 
 
 
-
-
-
-
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser 
 */
 // module.exports = { Bank };
--- d42class/savingsaccount.js
+++ d42class/savingsaccount.js
@@ -1,6 +1,7 @@
+/* eslint-disable require-jsdoc */
 "use strict";
-const acc = require("./account.js");//with node need the name of your file with your functions here
-const Account = acc.Account;  //do this for all of the functions used in the Mocha tests
+// const acc = require("./account.js");//with node need the name of your file with your functions here
+// const Account = acc.Account;  //do this for all of the functions used in the Mocha tests
 
 /* exports at end of file since exporting an object, which can only be referenced after definition   */
 
