b4b48cc4eff00f1a0dabacee021db93f6ca650d9
Barni_Classic <76923911+barniclassic@users.noreply.github.com>
2021-10-12 10:41:51 -0500

    remove

--- OOP-305/W1D2CoordFunc.js
+++
@@ -1,9 +0,0 @@
-let distance;
-let x1, y1, x2, y2;
-
-function calcDistance(x1, y1, x2, y2) {
-    distance = ((x2 - x1) ** (2) + (y2 - y1) ** (2)) ** (1 / 2);
-    return distance;
-}
-calcDistance(x1, y1, x2, y2)
-console.log("expect 7.07 : ", calcDistance(0, 0, 5, 5));
