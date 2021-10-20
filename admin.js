5fcaf6a7c1ffbda8e66eab6e269ce2e26b1a382d
Barni_Classic <76923911+barniclassic@users.noreply.github.com>
2021-10-20 01:38:09 -0500

    Update admin.js

--- admin.js
+++ admin.js
@@ -5,7 +5,7 @@ window.onload = function () {
     let password = document.getElementById("pwd1");
     if (email.value === "barniclassic@gmail.com" && password.value === "Hello123") {
       alert("Login successfully");
-      window.location.href = "./adminDash.html";
+      window.location.href = "https://barniclassic.github.io/adminDash.html";
     }else {
         alert("Your email and password combination is incorrect");
         warning.style.visibility = "visible"; 
